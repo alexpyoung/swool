2ebf7cc91b4c05741b57bebf59008471dc919663
Alex Young <alexyoung1992@gmail.com>
2024-04-27 16:56:18 -0400

    feat(xcodeproj): initial commit

---
+++ swool/swoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  swoolApp.swift
+//  swool
+//
+//  Created by Alex Young on 4/27/24.
+//
+
+import SwiftUI
+
+@main
+struct swoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
